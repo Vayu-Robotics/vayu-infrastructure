c447e2540aedd253da07fb59931d8bc4c4dcb4ca
Suyash Nigam <suyash@vayurobotics.com>
2024-08-26 15:36:24 -0700

    added metrics calc backbone

---
+++ src/airflow_dags/metrics_calculation/base_metrics_calculation.py
@@ -0,0 +1,44 @@
+""" Base metrics calculation class.
+1. The base class sets up the files to be read.
+2. Calls relevant callbacks to calculate metrics.
+3. Writes the calculated metrics to the databasee
+
+"""
+
+from math import isnan
+from pathlib import Path
+from typing import List
+import numpy as np
+import rdp
+import utm
+from mcap.reader import make_reader
+from mcap_ros2.reader import read_ros2_messages
+from tqdm import tqdm
+
+
+class MetricsCalculation:
+
+    def __init__(self, metric_class_dict : dict):
+        # Dict of {metric_name: metric_class}/
+        # metric_class should be a subclass of BaseMetricCalculation
+        self.metric_class_dict = metric_class_dict
+    
+    def write_metrics_to_db(self, metrics):
+        # Use psychopg2 to write metrics to the database
+        NotImplementedError
+
+    def compute_metrics(bags : List):
+        pass
+    
+
+
+class BaseMetricCalculation:
+
+    def __init__(self, topics_to_parse: List[str]):
+        self.topics_to_parse = topics_to_parse
+
+    def get_bag_iterator(self, mcap_file):
+        return read_ros2_messages(mcap_file, topics=self.topics_to_parse)
+    
+    def compute(self, mcap_file : str) -> dict:
+        raise NotImplementedError("Subclasses must implement this method")
